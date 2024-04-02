fe1803e4660d89fc83d3f21f25c5ac760e016069
Trong Tin <tran.trong.tin@sun-asterisk.com>
2024-04-02 10:05:05 +0700

    initial project

---
+++ src/config/ormconfig.ts
@@ -0,0 +1,17 @@
+import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+dotenv.config();
+const port: number = process.env.PORTDB ? parseInt(process.env.PORTDB) : 3306;
+
+export const AppDataSource = new DataSource({
+  type: 'mysql',
+  host: process.env.HOST || 'localhost',
+  port: port,
+  username: process.env.USERNAMEDB,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  synchronize: true,
+  logging: false,
+  entities: ['src/entities/**/*.ts'],
+  migrations: ['src/migrations/**/*.ts'],
+});
